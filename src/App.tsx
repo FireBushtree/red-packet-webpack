a2d1b959776b42207eb69e8e138cea845e1a7423
firebushtree <huangqian@chinacici.com>
2025-08-30 09:29:11 +0800

    feat: init repo

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App(props: any) {
+  return (
+    <div className="bg-amber-300 h-dvh">
+      {props.message}
+      <span>123</span>
+      <div>34123</div>
+    </div>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+import path from 'node:path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+
+const __dirname = path.resolve('./')
+
+const mode = 'development'
+const isProd = mode === 'production'
+
+export default {
+  cache: false,
+  mode,
+  entry: './src/main.tsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].[contenthash].css',
+    }),
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx', '.json'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 'swc-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: [isProd ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+}
